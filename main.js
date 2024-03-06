2662fa718d20b32be658767fb92ca9bc5bdb6016
Samantha Bennett <samanthabennett@ufl.edu>
2024-03-06 14:41:02 -0500

    added notes on password encryption

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 //used to ensure all the usernames for all the accounts are unqiue 
 const usernames = [];
 
+//use something like bycryt for password encryption
+
 //this will store the currently made accounts to make it easier to fine specific people based on their username
 const admins = [];
 const employees = [];
