e3a7d89fff6750c5b693d720fcfe4716179dd923
Samantha Bennett <samanthabennett@ufl.edu>
2024-04-07 20:11:16 -0400

    created view available cars func in main

--- main.js
+++ main.js
@@ -5,20 +5,25 @@ import User from './User'
 import Admin from './Admin'
 
 //used to ensure all the usernames for all the accounts are unqiue 
-const usernames = [];
+let usernames = [];
 
 //use something like bycryt for password encryption
 
 //this will store the currently made accounts to make it easier to fine specific people based on their username
 //remember to pass these into functions; if not return the lists
-const admins = [];
-const employees = [];
-const customers = [];
+let admins = [];
+let employees = [];
+let customers = [];
 
 //make list of all the cars on system
-const cars = [];
+let cars = [];
 
 //since every kind of account needs this function it will be declared in main not the classes
-function ViewAllCars(startDate, endDate){
-    
+function ViewAllCars(startDate, endDate, startTime, endTime){
+    let avail_cars = [];
+    for(let i = 0; i < cars.length; i++){
+        if(cars[i].CheckAvail(startDate, endDate, startTime, endTime)){
+            avail_cars.push(cars[i]);
+        }
+    }
 }
