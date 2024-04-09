c05afc59d742d717a7a6d992b31ff3683136faee
Samantha Bennett <117133024+samnbe@users.noreply.github.com>
2024-04-09 15:48:22 -0400

    added viewCar func

--- User.js
+++ User.js
@@ -2,11 +2,22 @@ class User{
     constructor(username, password, name) {
         //constructor 
         this.username = username; //check in database to ensure username & password are unqiue
-        this.password = password; //this has to go through a hash func before getting here
+        this.password = password; 
         this.name = name;
     }
 
     //Login functionality
+    ViewCars;
+}
+
+User.prototype.ViewCars = function(carsArray, startDate, endDate, startTime, endTime) {
+    let avail_cars = [];
+    for(let i = 0; i < carsArray.length; i++){
+        if(carsArray[i].CheckAvail(startDate, endDate, startTime, endTime)){
+            avail_cars.push(carsArray[i]);
+        }
+    }
+    return avail_cars;
 }
 
 module.export = User;
