63728ef02d501531ba47524d1ab18aea278de5ed
Samantha Bennett <117133024+samnbe@users.noreply.github.com>
2024-04-09 15:48:51 -0400

    remove viewCar func

--- main.js
+++ main.js
@@ -11,8 +11,3 @@ const customers = [];
 
 //make list of all the cars on system
 const cars = [];
-
-//since every kind of account needs this function it will be declared in main not the classes
-function ViewAllCars(){
-
-}
