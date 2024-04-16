dfb3cfd2f6ed0c61ced7ba2e26fd83365f88773a
Samantha Bennett <117133024+samnbe@users.noreply.github.com>
2024-04-16 18:57:19 -0400

    added sorting through cars for location

--- Employee.js
+++ Employee.js
@@ -7,7 +7,6 @@ class Employee extends User{
     constructor(username, password, name, location){
         super(username, password, name)
         this.location = location;
-        this.userType = "Employee";
     }
 
     getEmployeeUsername() {
@@ -36,36 +35,20 @@ class Employee extends User{
         return true;
     }
 
-    //view schedule of specific car 
-    viewReservations(carObj) {
-        return carObj.GetAvail()
-    }
-
-    //approving clients request online, client has to have an account 
-    //checking avail should be done before request gets here 
-    addReservation(startDate, endDate, startTime, endTime, clientUsername, carObj) {
-        carObj.AddReserve(startDate, endDate, startTime, endTime, clientUsername);
-    }
-
-    removeReservation(startDate, endDate, startTime, endTime, carObj) {
-        carObj.RemoveReserve(startDate, endDate, startTime, endTime);
-    }
-
-    checkForClientAccount(CustomerArray, clientUsername) {
-        for(let i = 0; i < CustomerArray.length; i++){
-            if(CustomerArray[i].GetCustUsername() == clientUsername){
-                return true;
+    searchCarFromLocation(carArray, location){
+        let carAtLocation = [];
+        for(let i = 0; i < carArray.length; i++){
+            let temp = carArray[i].GetInfo();
+            //console.log(temp[1])
+            if(temp[1] == location){
+                //console.log(temp[1])
+                carAtLocation.push(carArray[i])
             }
         }
-        return false;
+        return carAtLocation;
     }
 
-    createClientAccount(customerArray, clientName, clientUsername, password) {
-        //create client account if they don't have one when they first submit a reservation
-        customerArray.push(new Customers(clientUsername, password, clientName));
-    }
-
-     editCar(carArray, type, location, mileage, dayCost, mileCost, status, availability, editedElement, newVal) {
+    editCar(carArray, type, location, mileage, dayCost, mileCost, status, availability, editedElement, newVal) {
         const info = [type, location, mileage, dayCost, mileCost, status, availability];
         for (let i = 0; i < carArray.length; i++){
             if(JSON.stringify(info) === JSON.stringify(carArray[i].GetInfo())){
@@ -100,6 +83,34 @@ class Employee extends User{
         }
     }
 
+    //view schedule of specific car 
+    viewReservations(carObj) {
+        return carObj.GetAvail()
+    }
+
+    //approving clients request online, client has to have an account 
+    //checking avail should be done before request gets here 
+    addReservation(startDate, endDate, startTime, endTime, clientUsername, carObj) {
+        carObj.AddReserve(startDate, endDate, startTime, endTime, clientUsername);
+    }
+
+    removeReservation(startDate, endDate, startTime, endTime, carObj) {
+        carObj.RemoveReserve(startDate, endDate, startTime, endTime);
+    }
+
+    checkForClientAccount(CustomerArray, clientUsername) {
+        for(let i = 0; i < CustomerArray.length; i++){
+            if(CustomerArray[i].GetCustUsername() == clientUsername){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    createClientAccount(customerArray, clientName, clientUsername, password) {
+        //create client account if they don't have one when they first submit a reservation
+        customerArray.push(new Customers(clientUsername, password, clientName));
+    }
 }
 
 module.exports = Employee;
